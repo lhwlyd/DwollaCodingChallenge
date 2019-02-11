940548140eb93b39060b0d120cdf2b19fda4b079
lhwlyd <yil623@ucsd.edu>
2019-02-10 22:30:17 -0800

    fix 2

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "./setupTests";
+import "./setUpTests";
 import { shallow } from "enzyme";
 import sinon from "sinon";
 
